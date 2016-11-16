abfff838bbc10c034149b94946f169475d5b156c
Ant Cosentino <antony@mycosentino.com>
2016-11-16 12:26:12 +0200

    derp

--- android/src/main/java/za/co/io/sentry/ReactNativeSentryAndroidModule.java
+++ android/src/main/java/za/co/io/sentry/ReactNativeSentryAndroidModule.java
@@ -21,7 +21,7 @@ public final class ReactNativeSentryAndroidModule extends ReactContextBaseJavaMo
 
   public ReactNativeSentryAndroidModule(ReactApplicationContext rctx) {
     super(rctx);
-    Raven.init(rctx.getCurrentActivity());
+    Raven.init(rctx.super.getCurrentActivity());
   }
 
   @Override
